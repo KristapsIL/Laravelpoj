f4400253cf30a75b944370f151a346ea0061f3e1
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-18 16:55:33 +0300

    a'

---
+++ resources/views/products.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Products</title>
+</head>
+<body>
+    <h1>Products</h1>
+    <main>
+        <article>
+            <h2>Doctor's Sausage</h2>
+            <img src="/images/doctorsausage.jpg" alt="thesausage" width="200px"/>
+            <p>This is the tastiest sausage ever.
+            If you have never eaten this, you haven't lived!</p>
+            <P>1.99 EUR</P>
+        </article>
+    </main>
+</body>
+</html>
